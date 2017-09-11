7815efe2e851207e31f51c98756383306eb4b76d
0770724 <0770724@KELCE-ACIS11-P>
2017-09-11 17:57:22 -0500

    Added navigation as starting point. Added new page.

---
+++ XamarinSample/XamarinSample/AddedPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinSample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddedPage : ContentPage
+    {
+        public AddedPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BackButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
--- XamarinSample/XamarinSample/MainPage.xaml.cs
+++ XamarinSample/XamarinSample/MainPage.xaml.cs
@@ -13,5 +13,10 @@ namespace XamarinSample
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AddedPage());
+        }
     }
 }
